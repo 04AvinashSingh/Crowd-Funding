2466aa900864eee313a3e1fa07b123e0febbb634
04AvinashSingh <Avinash1704@outlook.com>
2024-07-10 22:45:49 +0530

    frontend

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { navlinks } from '../constants';
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
-      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+      <img src={imgUrl} alt="fund_logo" className="w-234 h-234" />
     ) : (
       <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
     )}
@@ -19,15 +19,15 @@ const Sidebar = () => {
   const [isActive, setIsActive] = useState('dashboard');
 
   return (
-    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+    <div className="flex justify-between items-center flex-col sticky top-1 h-[9vh] gap-40">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        <Icon styles="w-[100px] h-[100px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
-        <div className="flex flex-col justify-center items-center gap-3">
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-0 ">
+        <div className="flex flex-col justify-center items-center gap-5">
           {navlinks.map((link) => (
-            <Icon 
+            <Icon
               key={link.name}
               {...link}
               isActive={isActive}
@@ -41,8 +41,10 @@ const Sidebar = () => {
           ))}
         </div>
 
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        
       </div>
+      
+      <Icon styles="flex flex-col justify-center items-center" imgUrl={sun} />
     </div>
   )
 }
